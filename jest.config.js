3a86d323cbf8c03c7981520c8d40dffea7bb3f43
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 17:46:40 +0700

    Remove redundant module mappings

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 import { configureJest } from '@run-z/project-config';
 
-export default await configureJest({
-  moduleNameMapper: {
-    // Subpath imports not supported by Jest
-    // See https://github.com/facebook/jest/issues/11100
-    '^#ansi-styles$': 'ansi-styles',
-    '^#supports-color$': 'supports-color',
-  },
-});
+export default await configureJest();
