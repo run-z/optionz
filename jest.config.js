704dfc01d4d7c8e439afda407e492a8001403547
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 15:39:01 +0700

    Build with `@run-z/project-config`

--- jest.config.js
+++ jest.config.js
@@ -1,53 +1,10 @@
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/spec/**',
-    '!src/**/*.d.ts', // Exclude type definitions
-    '!src/**/*.spec.ts', // Exclude tests
-    '!src/**/index.ts',
-    '!src/**/main.ts',
-    '!**/node_modules/**',
-  ],
-  coverageDirectory: 'target/coverage',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
-  },
-  extensionsToTreatAsEsm: ['.ts'],
+import { configureJest } from '@run-z/project-config';
+
+export default await configureJest({
   moduleNameMapper: {
     // Subpath imports not supported by Jest
     // See https://github.com/facebook/jest/issues/11100
     '^#ansi-styles$': 'ansi-styles',
     '^#supports-color$': 'supports-color',
   },
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: '@run-z/optionz',
-        outputDirectory: './target/test-results',
-        classNameTemplate: '{classname}: {title}',
-        titleTemplate: '{classname}: {title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: 'true',
-      },
-    ],
-  ],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.spec.json',
-        useESM: true,
-      },
-    ],
-  },
-};
+});
